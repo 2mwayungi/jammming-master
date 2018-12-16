36a374f3c0ce2396b08fd64d94b45da27e74a790
King <2mwayungi@gmail.com>
2018-12-16 06:08:32 +0300

    Updates

---
+++ precache-manifest.fc545d15ead21177c395086196d992d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming-master/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "3e5db7c4fbc31fb39e26",
+    "url": "/jammming-master/static/js/runtime~main.3e5db7c4.js"
+  },
+  {
+    "revision": "5a781f2c595bc9177242",
+    "url": "/jammming-master/static/js/main.5a781f2c.chunk.js"
+  },
+  {
+    "revision": "77958300264c6869a197",
+    "url": "/jammming-master/static/js/1.77958300.chunk.js"
+  },
+  {
+    "revision": "5a781f2c595bc9177242",
+    "url": "/jammming-master/static/css/main.b57cd8b6.chunk.css"
+  },
+  {
+    "revision": "cf0e5ff81c583a4bca9ca97efb1b0c84",
+    "url": "/jammming-master/index.html"
+  }
+];
